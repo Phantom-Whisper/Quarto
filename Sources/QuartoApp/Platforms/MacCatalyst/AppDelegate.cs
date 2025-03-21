33f3b60b22575cf17b70dd6de69971334cd05c0e
Gabriel COUDEL-KOUMBA <gacoudelko>
2025-03-21 08:26:41 +0100

    :tada: création du projet QuartoApp

---
+++ Sources/QuartoApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QuartoApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
