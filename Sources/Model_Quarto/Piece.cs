85f1fb9fd450af7bfd6ce5348147b54946344e8f
Ange GRIMAUD <angrimaud>
2025-04-02 19:12:36 +0200

    :sparkles: Class Piece finie

---
+++ Sources/Model_Quarto/Piece.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Piece
+    {
+        public bool IsSquare
+        {
+            get;
+            init;
+        }
+        public bool IsLight
+        {
+            get;
+            init;
+        }
+        public bool IsBig
+        {
+            get;
+            init;
+        }
+        public bool IsFull
+        {
+            get;
+            init;
+        }
+
+        public Piece(bool isSquare, bool isLight, bool isBig, bool isFull)
+        {
+            IsSquare = isSquare;
+            IsLight = isLight;
+            IsBig = isBig;
+            IsFull = isFull;
+        }
+    }
+}
