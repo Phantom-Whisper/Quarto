a5681b32441a13edfac690a2015921485fce9193
Gabriel COUDEL-KOUMBA <gacoudelko>
2025-04-02 19:24:27 +0200

    ajout de classe Player

---
+++ Sources/Model_Quarto/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Player
+    {
+
+        public Player(string pseudo)
+        {
+            Pseudo = pseudo;
+        }
+
+        public required string Pseudo
+        {
+            get;
+            init;
+        }
+
+        private int nbWin;
+
+        public int NbWin
+        {
+            get => nbWin;
+            init
+            {
+                nbWin = 0;
+            }
+        }
+
+        public void AddWin()
+        {
+            ++nbWin;
+        }
+    }
+}
