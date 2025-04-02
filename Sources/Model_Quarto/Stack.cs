8b872ab174d82cdd1c9e6bb6aeb00ba509798c6a
Inès Delinac <ines.delinac@etu.uca@gmail.fr>
2025-04-02 19:12:28 +0200

    :construction: ajout Class Stack

---
+++ Sources/Model_Quarto/Stack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Model
+{
+    public class Stack
+    {
+        private ObservableCollection<int> stack { get; }
+
+        public Stack(ObservableCollection<int> stack) {  this.stack = stack; }
+
+        public bool IsEmpty() {  return stack.Count == 0; }
+
+        
+
+    }
+}
