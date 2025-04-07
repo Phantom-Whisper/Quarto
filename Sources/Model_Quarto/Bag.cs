7fd417efc1f4b3f95810d714e8536fbfa3354fa3
Inès Delinac <ines.delinac@etu.uca@gmail.fr>
2025-04-07 11:45:11 +0200

    :construction: modification en bag et ajout fonction

---
+++ Sources/Model_Quarto/Bag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Model
+{
+    public class Bag
+    {
+        public Bag()
+        {
+            Baglist = new ObservableCollection<Piece>();
+        }
+
+        private ObservableCollection<Piece> baglist;
+
+        public ObservableCollection<Piece> Baglist
+        {
+            get => baglist ??= new ObservableCollection<Piece>();
+            set => baglist = value ?? new ObservableCollection<Piece>();
+        }
+
+        public void AddPiece(bool isSquare, bool isLight, bool isBig, bool isFull)
+        {
+            Baglist.Add(new Piece(isSquare,isLight,isBig,isFull));
+        }
+
+        public void RemovePiece(Piece piece)
+        {
+            if (baglist != null)
+            {
+                Baglist.Remove(piece);
+            }
+            else
+            {
+                throw new InvalidOperationException("La pièce n'est pas dans la liste");
+            }
+            
+        }
+
+    }
+}
