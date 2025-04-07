99ed2752f4d9544f3417c024e503752a3e6935a0
Gabriel COUDEL-KOUMBA <gabriel.coudel-koumba@etu.uca.fr>
2025-04-07 23:42:51 +0200

    :memo: documentation pour les classe Board et Piece

--- Sources/Model_Quarto/Board.cs
+++ Sources/Model_Quarto/Board.cs
@@ -8,6 +8,11 @@ namespace Model
 {
     public class Board
     {
+        /// <summary>
+        /// This is the <c>Ctor</c> of the Class <c>Board</c>.
+        /// </summary>
+        /// <param name="x">Number of cells in the x-axis.</param>
+        /// <param name="y">Number of cells in the y-axis.</param>
         public Board(int x, int y) 
         {
             SizeX = x;
@@ -15,18 +20,34 @@ namespace Model
             grid = new Piece[SizeX, SizeY];
         }
 
+        /// <summary>
+        /// This property contains the number of cells in the x-axis.
+        /// </summary>
         public int SizeX
         {
             get;
         }
 
+        /// <summary>
+        /// This property contains the number of cells in the y-axis.
+        /// </summary>
         public int SizeY
         {
             get;
         }
 
+        /// <summary>
+        /// A grid of <c>Piece</c> making the <c>Board</c>.
+        /// </summary>
         public readonly Piece[,] grid;
 
+        /// <summary>
+        /// Thid method insert a piece at a certain position on the board.
+        /// </summary>
+        /// <param name="piece"></param>
+        /// <param name="x">Position on the x-axis.</param>
+        /// <param name="y">Position on the y-axis.</param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void InsertPiece(Piece piece, int x, int y)
         {
             if (IsEmpty(x, y) && IsOnBoard(x, y))
@@ -35,6 +56,12 @@ namespace Model
                 throw new InvalidOperationException("The piece cannot be placed in this position.");
         }
 
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current state of the <c>Board</c>.
+        /// </returns>
         public override string ToString()
         {
             string str = "";
@@ -102,6 +129,12 @@ namespace Model
             return str;
         }
 
+        /// <summary>
+        /// This method tells if the cell at a certain position is empty or not
+        /// </summary>
+        /// <param name="x">Position on the x-axis</param>
+        /// <param name="y">Position on the y-axis</param>
+        /// <returns></returns>
         public bool IsEmpty(int x, int y)
         {
             if (grid[x, y] == null)
@@ -109,8 +142,20 @@ namespace Model
             return false;
         }
 
+        /// <summary>
+        /// This method returns a piece at a specific position on the board
+        /// </summary>
+        /// <param name="x">Position on the x-axis</param>
+        /// <param name="y">Position on the y-axis</param>
+        /// <returns></returns>
         public Piece GetPiece(int x, int y) => grid[x, y];
 
+        /// <summary>
+        /// This method tells if the coordinates are within the limits of the board
+        /// </summary>
+        /// <param name="x">Position on the x-axis</param>
+        /// <param name="y">Position on the y-axis</param>
+        /// <returns></returns>
         public bool IsOnBoard(int x, int y)
         {
             if (x < 0 || y < 0 || x > SizeX || y > SizeY)
@@ -118,6 +163,9 @@ namespace Model
             return true;
         }
 
+        /// <summary>
+        /// This method removes the board of all the <c>pieces</c> on it
+        /// </summary>
         public void ClearBoard()
         {
             Array.Clear(grid, 0, grid.Length);
