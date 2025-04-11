ac40d64d36c324267635d24cc29c4845312fb2d4
Gabriel COUDEL-KOUMBA <gacoudelko>
2025-04-11 09:53:19 +0200

    ajouts GameManager

--- Sources/Model_Quarto/IGameManager.cs
+++ Sources/Model_Quarto/IGameManager.cs
@@ -13,18 +13,33 @@ namespace Model
 
     public class GameManager : IGameManager
     {
-        public void LaunchGame() 
-        {
-            public readonly Board board;
+        readonly Board board;
 
-            Player[] players;
-            Player player;
-            
-            Bag bag;
+        Player[] players;
+        Player player;
+
+        Bag bag;
+
+        GameManager(Board board, Bag bag)
+        {
+            this.board = board;
+            this.bag = bag;
+            //CreatePlayers();
+        }
+        public void LaunchGame()
+        {
+
 
             //Méthode pour sélectionner les noms de joueurs
 
 
+        }
+
+
+        private void CreatePlayers(string nom1, string nom2)
+        {
+            players[0] = new Player(nom1);
+            players[1] = new Player(nom2);
         }
     }
 }
