0cc29bdf6651596bff9c67ab097d841b42668f4f
Inès Delinac <ines.delinac@etu.uca@gmail.fr>
2025-04-11 09:17:48 +0200

    :memo: update of the documentation

--- Sources/Model_Quarto/Bag.cs
+++ Sources/Model_Quarto/Bag.cs
@@ -5,18 +5,37 @@ namespace Model
 {
     public class Bag
     {
+
+        /// <summary>
+        /// This is the <c>Ctor</c> of the Class <c>Bag</c>.
+        /// </summary>
         public Bag()
         {
             Baglist = new ObservableCollection<Piece>();
         }
 
+        /// <summary>
+        /// This property contains the collection of <c>Piece</c> that the player can put on the <c>Board</c>.
+        /// </summary>
         public ObservableCollection<Piece> Baglist { get; private set; }
 
+        /// <summary>
+        /// This method create and add a <c>Piece</c> to the <c>Bag</c>.
+        /// </summary>
+        /// <param name="isSquare"> boolean of the shape : true = Square and false = Round </param>
+        /// <param name="isLight"> boolean of the color : true = Light and false = Dark </param>
+        /// <param name="isBig"> boolean of the size : true = Big and false = Small </param>
+        /// <param name="isFull"> boolean of the state : true = Full and false = Hollow </param>
         public void AddPiece(bool isSquare, bool isLight, bool isBig, bool isFull)
         {
             Baglist.Add(new Piece(isSquare,isLight,isBig,isFull));
         }
 
+        /// <summary>
+        /// This method remove a <c>Piece</c> from the <c>Bag</c>.
+        /// </summary>
+        /// <param name="piece"> <c>Piece</c> removed </param>
+        /// <exception cref="InvalidOperationException"> when the piece is not in the collection </exception>
         public void RemovePiece(Piece piece)
         {
             if (Baglist != null)
@@ -25,7 +44,7 @@ namespace Model
             }
             else
             {
-                throw new InvalidOperationException("La pièce n'est pas dans la liste");
+                throw new InvalidOperationException("The piece is not in the bag");
             }
             
         }
--- Sources/Model_Quarto/PlayerIA.cs
+++ Sources/Model_Quarto/PlayerIA.cs
@@ -3,6 +3,9 @@ namespace Model
 {
     public class PlayerIA : Player
     {
+        /// <summary>
+        /// This is the <c>Ctor</c> of the Class <c>PlayerIA</c> with the pseudo : IA.
+        /// </summary>
         public PlayerIA() : base("IA")
         {
         }
