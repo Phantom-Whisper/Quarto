bd10f35a44b089c5558c67b634bd05e96a3ca6b1
Gabriel COUDEL-KOUMBA <gacoudelko>
2025-04-11 09:52:21 +0200

    :construction: interface GameManager

---
+++ Sources/Model_Quarto/IGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Model
+{
+    public interface IGameManager
+    {
+        void LaunchGame();
+
+        //void SaveGame();
+
+        //void LoadGame();
+    }
+
+    public class GameManager : IGameManager
+    {
+        public void LaunchGame() 
+        {
+            public readonly Board board;
+
+            Player[] players;
+            Player player;
+            
+            Bag bag;
+
+            //Méthode pour sélectionner les noms de joueurs
+
+
+        }
+    }
+}
