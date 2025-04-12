80d54aabccd05a14da4c44dd0b4b409e78a2ce1b
Inès Delinac <ines.delinac@etu.uca@gmail.fr>
2025-04-12 09:53:48 +0200

    :white_check_mark: ajout des test du Ctor, Add et Remove

---
+++ Sources/TestModel/TestBag.cs
@@ -0,0 +1,50 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel
+{
+    public class TestBag
+    {
+
+        [Fact]
+        public void TestCtor()
+        {
+            Bag bag = new Bag();
+            Assert.NotNull(bag);
+        }
+
+
+        [Theory]
+        [InlineData(true, true,true,true)]
+        [InlineData(false, false, true, true)]
+        [InlineData(true, true, false, false)]
+        public void TestAddPiece(bool isSquare, bool isLight, bool isBig, bool isFull)
+        {
+            Bag bag = new Bag();
+            Piece piece = new Piece(isSquare, isLight, isBig, isFull);
+
+            bag.AddPiece(isSquare, isLight, isBig, isFull);
+
+            Assert.NotEmpty(bag.Baglist);
+        }
+
+        [Theory]
+        [InlineData(true, true, true, true)]
+        [InlineData(false, false, true, true)]
+        [InlineData(true, true, false, false)]
+        public void TestRemovePiece_OK(bool isSquare, bool isLight, bool isBig, bool isFull)
+        {
+            Bag bag = new Bag();
+            Piece piece = new Piece(isSquare, isLight, isBig, isFull);
+
+            bag.AddPiece(isSquare, isLight, isBig, isFull);
+            bag.RemovePiece(piece);
+
+            Assert.DoesNotContain(piece,bag.Baglist);
+        }
+    }
+}
