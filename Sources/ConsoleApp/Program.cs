f8e19fdb87282d20423af1eb9a0135a30bc0d3af
Gabriel COUDEL-KOUMBA <gabriel.coudel-koumba@etu.uca.fr>
2025-04-23 17:43:30 +0200

    :adhesive_bandage: Fix class Player, PlayerIA :sparkles: Class PlayerHumain

--- Sources/ConsoleApp/Program.cs
+++ Sources/ConsoleApp/Program.cs
@@ -7,7 +7,13 @@ Piece piece = new Piece(true, true, true, false);
 
 board.InsertPiece(piece, 2,2);
 
-Dictionary<string, int> dP1 =  board.PositionPiece(piece);
-Console.WriteLine(dP1["y"]);
+//Dictionary<string, int> dP1 =  board.PositionPiece(piece);
+//Console.WriteLine(dP1["y"]);
 
-
+PlayerIA playerIA = new();
+PlayerHumain playerHumain = new("joueur");
+var win = playerIA.NbWin;
+Console.WriteLine(playerIA.Pseudo);
+Console.WriteLine(win);
+Console.WriteLine(playerHumain.Pseudo);
+Console.WriteLine(playerHumain.NbWin.ToString());
