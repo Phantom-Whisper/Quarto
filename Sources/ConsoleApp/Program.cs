b39545513e48432790c3e2b187b99da2e7fab227
Gabriel COUDEL-KOUMBA <gacoudelko>
2025-05-09 09:34:35 +0200

    fix security error

--- Sources/ConsoleApp/Program.cs
+++ Sources/ConsoleApp/Program.cs
@@ -4,9 +4,7 @@
 Board board = new(4, 4);
 
 Piece piece = new Piece(true, true, true, false);
-var ia = new StubAI();
 board.InsertPiece(piece, 2,2);
-var list = ia.GetAvailablePositions(board);
 
 //Dictionary<string, int> dP1 =  board.PositionPiece(piece);
 //Console.WriteLine(dP1["y"]);
@@ -20,8 +18,7 @@ Console.WriteLine(win);
 Console.WriteLine(playerHumain.Pseudo);
 Console.WriteLine(playerHumain.NbWin.ToString());
 Console.WriteLine("-----------------------------------------------");
-foreach(var item in list) { Console.WriteLine(item); }
-ia.Move(board, piece);
+
 Console.WriteLine(board.ToString());
 Console.WriteLine("----------------------------------");
 Piece piecee = new(true,false, true, true);
