4533805b6020f05cfa151404089c51145532e421
Inès Delinac <ines.delinac@etu.uca@gmail.fr>
2025-05-16 14:49:12 +0200

    :construction: QuartoEventArgs added

---
+++ Sources/Manager/CustomEventArgs/QuartoEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Manager.CustomEventArgs
+{
+    public class QuartoEventArgs(IRulesManager rules, IBoard board, IPlayer currentPlayer) : EventArgs
+    {
+        
+        public IRulesManager RulesManager { get; } = rules;
+        public IBoard Board { get; } = board;
+        public IPlayer CurrentPlayer { get; } = currentPlayer;
+    }
+}
