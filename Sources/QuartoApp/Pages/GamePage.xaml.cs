c3187947b68a280d857451233cf6c89e262bf037
Ange GRIMAUD <angrimaud>
2025-05-23 15:29:58 +0200

    :poop: array à finir

--- Sources/QuartoApp/Pages/GamePage.xaml.cs
+++ Sources/QuartoApp/Pages/GamePage.xaml.cs
@@ -1,9 +1,12 @@
 namespace QuartoApp.Pages;
+using Model;
 
 public partial class GamePage : ContentPage
 {
-	public GamePage()
-	{
-		InitializeComponent();
-	}
+    public Board Matrix { get; } = new Board(4, 4);
+    public GamePage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
 }
