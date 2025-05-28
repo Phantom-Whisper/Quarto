7b72264ac7986c3bba715df78d9c28232561d2cd
Ange GRIMAUD <angrimaud>
2025-05-28 19:28:09 +0200

    :sparkles: add board

--- Sources/QuartoApp/Pages/GamePage.xaml.cs
+++ Sources/QuartoApp/Pages/GamePage.xaml.cs
@@ -1,13 +1,36 @@
 namespace QuartoApp.Pages;
+
+using System.Data.Common;
+using System.Text.Json;
+using Manager;
 using Model;
 using QuartoApp.MyLayouts;
 
 public partial class GamePage : ContentPage
 {
-    public Matrix2d Matrix { get; } = new Matrix2d(4, 4);
+    public Board board { get; } = new Board(4, 4);
     public GamePage()
     {
         InitializeComponent();
         BindingContext = this;
+    }
+    public IEnumerable<IPiece> FlatMatrix2d
+    {
+        get
+        {
+            List<IPiece> flatMatrix = new();
+
+            if (board.Grid == null) return flatMatrix;
+
+            IPiece[,] mat = new IPiece[board.SizeX, board.SizeY];
+            for (int numRow = 0; numRow < board.SizeX; numRow++)
+            {
+                for (int numCol = 0; numCol < board.SizeY; numCol++)
+                {
+                    flatMatrix.Add(board.Grid[numRow, numCol]);
+                }
+            }
+            return flatMatrix;
+        }
     }
 }
